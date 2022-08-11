a4b2d83871d1eed943c2926e1e86fca7cd422282
Artemy Gorchakov <rick-flat@mail.ru>
2022-08-11 14:53:33 +0300

    пустой вариант теста

---
+++ src/test/java/tests/YaTests.java
@@ -0,0 +1,13 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class YaTests {
+
+
+}
+
